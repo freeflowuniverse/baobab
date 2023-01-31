bfad56b796fc37efda638b344343e511ab6a7a0a
timurgordon <timurgordon@gmail.com>
2023-01-31 13:38:00 +0300

    fixed  client test

--- baobab/client/client_test.v
+++ baobab/client/client_test.v
@@ -1,22 +1,34 @@
 module client
 
+import time
 import os
 import freeflowuniverse.crystallib.texttools
+import freeflowuniverse.crystallib.redisclient
 import freeflowuniverse.baobab.actions
 
-const testpath = os.dir(@FILE) + '/../testdata'
+const samples_dir = os.dir(@FILE) + '/../../examples/actionsdir'
 
+// reset redis on test begin and run servers
+fn testsuite_begin() {
+	os.execute('redis-server --daemonize yes &')
+	mut redis := redisclient.core_get()
+	redis.flushall()!
+	redis.disconnect()
+}
+
+// reset redis on test end
+fn testsuite_end() {
+	mut redis := redisclient.core_get()
+	redis.flushall()!
+	redis.disconnect()
+}
 
 fn test_get_jobs() {
-	
-	mut cl:=new() or {panic("can't get client")}
+	mut cl := new() or { panic("can't get client") }
 
-	mut actionsmgr:=actions.dir_parse(testpath)!
+	mut actionsmgr := actions.dir_parse(samples_dir)!
 	assert actionsmgr.actions.len == 11
 
-	mut j:=cl.schedule_actions(actions:actionsmgr.actions)
-	assert j.jobs.last().action=="books.mdbook_develop"
-
-	
-
+	mut j := cl.schedule_actions(actions: actionsmgr.actions)
+	assert j.jobs.last().action == 'books.mdbook_develop'
 }
