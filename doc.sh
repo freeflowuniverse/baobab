f36af2d9391817167c9ad5f3f1a0b648948b23d8
brandonpille <brandon@threefold.tech>
2023-03-22 15:21:01 +0100

    Fixed yaml file

--- doc.sh
+++ doc.sh
@@ -1,19 +1,15 @@
 set -ex
 
+rm -rf docs/_docs
+rm -rf docs/_docs/v
+
 pushd manual
 bash run.sh
 popd
 
-
-
-rm -rf docs/_docs
-rm -rf docs/_docs/v
 v fmt -w baobab
 pushd baobab
 v doc -m -f html . -readme -comments -no-timestamp 
 popd
 
 mv baobab/_docs docs/v/
-open docs/v/index.html
-open docs/index.html
-
