23f24daa7f80697a2cec384b950d91861a8696ed
timurgordon <timurgordon@gmail.com>
2023-03-29 09:11:03 +0300

    fix smartfind

--- baobab/modelbase/smartfind.v
+++ baobab/modelbase/smartfind.v
@@ -4,7 +4,7 @@ module modelbase
 //walk over all targets, return the one which has cid or name match
 //if more than 1 found, then is error
 //QUESTION:return index or reference?
-fn cid_name_find[T](targets []T, cid_name string) !int {
+pub fn cid_name_find[T](targets []T, cid_name string) !int {
 	mut result := -1
 	for i, target in targets {
 		if !cid_name_matches[T](target, cid_name) {
@@ -18,11 +18,14 @@ fn cid_name_find[T](targets []T, cid_name string) !int {
 	return result
 }
 
+//BACKLOG: prettify
 fn cid_name_matches[T](target T, cid_name string) bool {
 	$for field in T.fields {
-		if field.name == 'cid' || field.name == 'name' {
-			if target.$(field.name) == cid_name {
-				return true
+		$if field.name == 'cid' || field.name == 'name' {
+			$if field.typ is string {
+				if target.$(field.name) == cid_name {
+					return true
+				}
 			}
 		}
 	}
